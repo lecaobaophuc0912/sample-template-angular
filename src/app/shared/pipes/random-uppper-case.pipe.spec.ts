937fe03cae523c00032f8e0c6581fe2d741331a7
Phuc Le <phuc.baocao@zaitenllc.com>
2021-08-06 12:43:38 +0700

    Complete pip unit test

--- src/app/shared/pipes/random-uppper-case.pipe.spec.ts
+++ src/app/shared/pipes/random-uppper-case.pipe.spec.ts
@@ -1,11 +1,32 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { waitForAsync } from '@angular/core/testing';
 import { RandomUppperCasePipe } from './random-uppper-case.pipe';
 
 describe('Pipe: RandomUppperCasee', () => {
-  it('create an instance', () => {
-    let pipe = new RandomUppperCasePipe();
-    expect(pipe).toBeTruthy();
-  });
+  let randomUpperCasePipe: RandomUppperCasePipe;
+  beforeEach(waitForAsync(() => {
+    randomUpperCasePipe = new RandomUppperCasePipe();
+  }))
+
+  it('create an instance', waitForAsync(() => {
+    expect(randomUpperCasePipe).toBeTruthy();
+  }));
+
+  it('should get arrUpperIndex less than length value', waitForAsync(() => {
+    expect(randomUpperCasePipe.transform('abcEXtA', [1, 3, 5])).toBe('aBcEXTA');
+  }));
+
+  it('should get arrUpperIndex more than length value', waitForAsync(() => {
+    expect(randomUpperCasePipe.transform('abcEXtA', [1, 2, 3, 4, 5, 6, 7, 8])).toBe('aBCEXTA');
+  }));
+
+  it('should get arrUpperIndex have value more than length value', waitForAsync(() => {
+    expect(randomUpperCasePipe.transform('abcEXtA', [1, 2, 3, 9])).toBe('aBCEXtA');
+  }));
+
+  it('should get arrUpperIndex is Empty Array', waitForAsync(() => {
+    expect(randomUpperCasePipe.transform('abcEXtA')).toBe('abcEXtA');
+  }));
+
 });
--- src/app/shared/pipes/random-uppper-case.pipe.ts
+++ src/app/shared/pipes/random-uppper-case.pipe.ts
@@ -7,9 +7,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RandomUppperCasePipe implements PipeTransform {
 
   transform(value: string, arrUpperIndex: Array<number> = []): string {
+    if (arrUpperIndex.length > value.length) {
+      arrUpperIndex.length = value.length;
+    }
     let arrString = Array.from(value);
     arrUpperIndex.forEach((index: number) => {
-      arrString[index] = arrString[index].toUpperCase();
+      if (arrString[index]) {
+        arrString[index] = arrString[index].toUpperCase();
+      }
     });
 
     return arrString.join('');
